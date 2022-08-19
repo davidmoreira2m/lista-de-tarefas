506ffb066b61e8da29657f778fa86604b493f810
David Moreira <davidmoreiramaia@gmail.com>
2022-08-19 16:54:37 -0300

    Atualização Lista de tarefas

--- script.js
+++ script.js
@@ -2,30 +2,49 @@ const inputText = document.querySelector("#input-text");
 const addButton = document.querySelector(".add-button");
 const taskListContainer = document.querySelector(".task-list-container");
 
-function addNewTask() {
+const addNewTask = () => {
    if (!inputText.value.trim()) {
+      inputText.classList.add("error");
+      inputText.value = "";
       return;
    }
 
    const task = document.createElement("div");
-   taskListContainer.appendChild(task);
    task.classList.add("task");
+   taskListContainer.appendChild(task);
+
+   const taskCheckBox = document.createElement("input");
+   taskCheckBox.setAttribute("type", "checkbox");
+   taskCheckBox.classList.add("task-checkbox");
+   task.appendChild(taskCheckBox);
 
    const taskText = document.createElement("p");
-   task.appendChild(taskText);
    taskText.innerText = inputText.value;
+   taskText.classList.add("task-text");
+   task.appendChild(taskText);
 
-   const taskIcon = document.createElement("span");
+   const taskIcon = document.createElement("i");
+   taskIcon.classList.add("fa-solid");
+   taskIcon.classList.add("fa-trash");
    task.appendChild(taskIcon);
-   taskIcon.innerText = "X";
 
    inputText.value = "";
 
-   // Remover
-   function removeTask() {
+   taskCheckBox.addEventListener("change", () => {
+      if (taskCheckBox.checked === true) {
+         taskText.classList.add("done");
+      } else {
+         taskText.classList.remove("done");
+      }
+   });
+
+   taskIcon.addEventListener("click", () => {
       task.remove();
-   }
-   taskIcon.addEventListener("click", removeTask);
-}
+   });
+};
 
 addButton.addEventListener("click", addNewTask);
+
+inputText.addEventListener("focus", () => {
+   inputText.classList.remove("error");
+});
